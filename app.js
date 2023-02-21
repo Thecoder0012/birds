70aaa2d55e590b64432d9bc6171a3e1b4deb1f8c
Thecoder0012 <mardineliassi57@gmail.com>
2023-02-21 21:21:04 +0100

    crud done except update

--- app.js
+++ app.js
@@ -1,19 +1,58 @@
-const express = require('express'); // import
+const express = require("express"); // import
 const app = express(); // instantiate
 
-const birds = [
-    {id: 1, name: "Starling"}
-];
+let id = 0;
+app.use(express.json()); // parses json from request bodies.
 
-app.get("/birds",(req,res) => {
-    res.send({data: birds});
+// empty bird array
+const birds = [];
+
+// returns new bird object
+function createBird(id, name, color) {
+  return (newBird = {
+    id: id,
+    name: name,
+    color: color,
+  });
+}
+
+// returns all birds
+function getAllBirds(birds) {
+  return birds;
+}
+
+// get all birds
+app.get("/birds", (req, res) => {
+  res.send({
+    data: getAllBirds(birds),
+  });
 });
 
-app.get("/birds/:id",(req,res) => {
-    const foundBird = birds.find(bird => bird.id === Number(req.params.id));
-    res.send({data: foundBird});
+// get bird
+app.get("/birds/:id", (req, res) => {
+  const foundBird = birds.find((bird) => bird.id === Number(req.params.id));
+  res.send({
+    data: foundBird,
+  });
 });
 
-app.listen(8080,() => {
-    console.log("Server started on port",8080);
-})
+// add new bird
+app.post("/birds", (req, res) => {
+  id++;
+  const newBird = createBird(id, req.body.name, req.body.color);
+  birds.push(newBird);
+  res.send(req.body);
+});
+
+// delete bird
+app.delete("/birds/:id", (req, res) => {
+  const bird = birds.find((bird) => bird.id === Number(req.params.id));
+  const birdIndex = birds.indexOf(bird);
+  birds.splice(birdIndex, 1);
+  res.send(bird);
+});
+
+// start port on 8080
+app.listen(8080, () => {
+  console.log("Server started on port", 8080);
+});
